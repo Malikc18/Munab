9560fce9ff06b104d992533e6243b11542c5246c
malik <ma-ilk3@h0tmail.com>
2026-04-27 21:12:10 +0300

    fix google redirect

--- app/login/page.js
+++ app/login/page.js
@@ -52,7 +52,7 @@ export default function Login() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/dashboard`,
+        redirectTo: `https://munab.app/dashboard`,
       },
     });
   };
